e8e17c3edc9750a600a4c88d4402b46094aae5c7
Andres Roldan <dresroldan@gmail.com>
2020-11-10 16:12:30 -0600

    Added port info for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 2080;
+const PORT = process.env.PORT || 2080;
 
 const app = express();
 
